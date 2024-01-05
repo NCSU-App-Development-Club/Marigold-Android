8ad6034d14214c0cecf52c5af9fa4a5e399d57dc
Peter Ising <pmising@ncsu.edu>
2024-01-05 14:52:50 -0500

    Adding OnboardingTextField to common composables file

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/composables.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/composables.kt
@@ -4,15 +4,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -21,6 +31,8 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -87,4 +99,53 @@ fun InvalidBanner(text: String?, modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.padding(4.dp))
         Text(text ?: "", color = MaterialTheme.colorScheme.error)
     }
+}
+
+@Composable
+fun OnboardingTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    modifier: Modifier = Modifier,
+    isError : Boolean = false,
+    password: Boolean = false,
+    label: String = "",
+) {
+    var passwordVisibility by remember { mutableStateOf(password) }
+
+    androidx.compose.material3.TextField(
+        value = value,
+        isError = isError,
+        visualTransformation = if (!passwordVisibility)
+            VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = keyboardOptions,
+        singleLine = true,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        shape = MaterialTheme.shapes.medium,
+        trailingIcon = if (password) {
+            {
+                IconButton(onClick = {
+                    passwordVisibility = !passwordVisibility
+                    // log output kt
+                }, modifier = Modifier.padding(end = 8.dp)) {
+                    Icon(
+                        imageVector = if (passwordVisibility)
+                            Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
+                        contentDescription = "Visibility",
+                        tint = Color(0xffbfbfbf),
+                    )
+                }
+            }
+        } else null,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier)
+    )
 }
