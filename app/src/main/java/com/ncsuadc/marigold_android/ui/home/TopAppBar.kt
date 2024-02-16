e58d80bb73e5e3f3d2f7f18591fb354853ab76b3
Abel Lu <aluprof4@gmail.com>
2024-02-15 20:47:30 -0500

    finished top app bar

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/TopAppBar.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/TopAppBar.kt
@@ -1,5 +1,9 @@
 package com.ncsuadc.marigold_android.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Image
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -9,13 +13,20 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ncsuadc.marigold_android.R
 
 
@@ -23,47 +34,42 @@ val cevicheFamily = FontFamily(
     Font(R.font.ceviche_one, FontWeight.Normal)
 )
 
-//val provider = GoogleFont.Provider(
-//    providerAuthority = "com.google.android.gms.fonts",
-//    providerPackage = "com.google.android.gms",
-//    certificates = R.array.com_google_android_gms_fonts_certs
-//)
-//
-//val fontName = GoogleFont("Ceviche One")
-//
-//val fontFamily = FontFamily(
-//    Font(googleFont = fontName, fontProvider = provider)
-//)
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun TopAppBar() {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.White,
-                    titleContentColor = Color(0xffffb320),
-                ),
-                title = {
-                    Text(
-                        "Marigold",
-                        fontFamily = cevicheFamily,
-                        fontWeight = FontWeight.Normal,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+            titleContentColor = Color(0xffffb320),
+        ),
+        title = {
+            Text(
+                "Marigold",
+                fontFamily = cevicheFamily,
+                fontWeight = FontWeight.Normal,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 48.sp
 
-                    )
-                },
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Image,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                }
             )
+        },
+        actions = {
+            IconButton(
+                onClick = { /* do something */ },
+                modifier = Modifier.padding(horizontal = 24.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.xavier_pfp),
+                    contentDescription = "Localized description",
+                    modifier = Modifier
+                        .height(48.dp)
+                        .width(48.dp)
 
+                )
+            }
+        }
+    )
 }
 
 
