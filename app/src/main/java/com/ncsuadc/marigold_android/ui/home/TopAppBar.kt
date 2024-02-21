b9d7569f01ea6aa84ed382ec13f372246874e4bd
Peter Ising <pmising@ncsu.edu>
2024-02-20 19:19:00 -0500

    [Style] Adding descriptor comments to TopAppBar

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/TopAppBar.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/TopAppBar.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ncsuadc.marigold_android.R
 
-
+//Font family value for the "Ceviche One" font in resources
 val cevicheFamily = FontFamily(
     Font(R.font.ceviche_one, FontWeight.Normal)
 )
@@ -32,9 +32,11 @@ val cevicheFamily = FontFamily(
 @Preview
 @Composable
 fun TopAppBar() {
+    //CenterAlignedTopApp bar component holds the content of the component
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color.White,
+            //Color is the Marigold yellow theme
             titleContentColor = Color(0xffffb320),
         ),
         title = {
@@ -48,14 +50,18 @@ fun TopAppBar() {
 
             )
         },
+        //Profile picture icon button, click functionality currently unimplemented
         actions = {
             IconButton(
                 onClick = { /* do something */ },
+                //Modifier controlling distance of the pic from the right side of the bar
                 modifier = Modifier.padding(horizontal = 24.dp)
             ) {
                 Image(
+                    //Painter to hold the drawable, current "xavier_pfp" in resources as placeholder
                     painter = painterResource(R.drawable.xavier_pfp),
                     contentDescription = "Localized description",
+                    //Modifier changing the size of the picture
                     modifier = Modifier
                         .height(48.dp)
                         .width(48.dp)
