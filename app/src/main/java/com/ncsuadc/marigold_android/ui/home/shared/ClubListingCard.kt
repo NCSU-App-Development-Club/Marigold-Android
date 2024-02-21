eaebf05299eda025de863ae1b9e54bc2e10b6b8c
presslerwill <presslerwill@gmail.com>
2024-02-20 19:38:57 -0500

    Added the rows and columns and club logo

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
@@ -1,2 +1,56 @@
 package com.ncsuadc.marigold_android.ui.home.shared
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ncsuadc.marigold_android.R
+import com.ncsuadc.marigold_android.domain.Club
+import com.ncsuadc.marigold_android.domain.Event
+import com.ncsuadc.marigold_android.ui.home.SmallEventCard
+
+@Composable
+fun ClubListingCard(club: Club) {
+    Card() {
+        Row() {
+            Column() {
+                Image(painter = painterResource(id = R.drawable.uab_logo),
+                    contentDescription = "UAB logo",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    alignment = Alignment.Center)
+            }
+
+            Column() {
+                Text(
+                    text = "I'm in the column",)
+            }
+            Column() {
+
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun OneCard() {
+    ClubListingCard(Club())
+
+}
