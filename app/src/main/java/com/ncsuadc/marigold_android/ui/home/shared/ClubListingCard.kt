c1e4fcd439a392d41ff93c171b665abcf6d15b1b
Monica Jin <94765861+monjinjin@users.noreply.github.com>
2024-02-20 20:06:26 -0500

    [Implementation] Edited columns & buttons

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
@@ -1,13 +1,17 @@
 package com.ncsuadc.marigold_android.ui.home.shared
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,9 +43,18 @@ fun ClubListingCard(club: Club) {
             Column() {
                 Text(
                     text = "I'm in the column",)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = "Subcolumn text",)
             }
-            Column() {
 
+            Column() {
+                @Composable
+                fun FilledTonalButtonExample(onClick: () -> Unit) {
+                    FilledTonalButton(onClick = { onClick() }) {
+                        Text("Join")
+                    }
+                }
             }
         }
 
