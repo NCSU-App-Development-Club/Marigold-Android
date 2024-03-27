64a945ecb54af809859237914b49164440aa3eaf
Arsalaan Khan <askhan6@ncsu.edu>
2024-03-26 20:37:21 -0400

    Added dummy button

--- app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
+++ app/src/main/java/com/ncsuadc/marigold_android/ui/home/shared/ClubListingCard.kt
@@ -3,18 +3,22 @@ package com.ncsuadc.marigold_android.ui.home.shared
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +28,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +40,9 @@ import com.ncsuadc.marigold_android.ui.home.SmallEventCard
 
 @Composable
 fun ClubListingCard(club: Club) {
-    Card {
+    Card (colors = CardDefaults.cardColors(
+        containerColor = Color.White,))
+    {
         Row() {
             Column() {
                 Image(painter = painterResource(id = R.drawable.uab_logo),
@@ -48,7 +56,7 @@ fun ClubListingCard(club: Club) {
 
             Spacer(modifier = Modifier.width(6.dp))
             
-            Column() {
+            Column(modifier=Modifier.weight(0.75f)) {
                 Row() {
                     Text(
                         text = "University Activities Board",
@@ -74,10 +82,15 @@ fun ClubListingCard(club: Club) {
 
             }
 
-            Column(verticalArrangement = Arrangement.Center) {
+            Column(verticalArrangement = Arrangement.Center, modifier = Modifier.align(alignment = Alignment.CenterVertically)) {
                 FilledTonalButtonExample()
             }
+            Spacer(modifier = Modifier.width(13.dp))
+
+
+
         }
+
 
     }
 
@@ -88,14 +101,21 @@ fun FilledTonalButtonExample() {
     FilledTonalButton(
         onClick = { null },
         modifier = Modifier
-            .height(20.dp)
-            .width(100.dp),
+            .height(30.dp)
+            .width(80.dp),
+
+        contentPadding = PaddingValues(0.dp),
         shape = ButtonDefaults.filledTonalShape,
-        colors = ButtonDefaults.filledTonalButtonColors()
+        colors = ButtonDefaults.filledTonalButtonColors(containerColor = Color.LightGray)
+
     ) {
-        Text(text = "Join",
-            fontSize = 12.sp,
-            color = Color.Black)
+        Text(text = "JOIN",
+            fontSize = 17.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(alignment = Alignment.CenterVertically)
+        )
     }
 }
 
